f8e8dfe77a4cc864c616b77820e678cc76a2b7f3
engrzahidriaz <zahidriaz371@gmail.com>
2021-01-17 12:45:15 +0500

    add new file

---
+++ myfirstRprogram.R
@@ -0,0 +1,25 @@
+a = 5
+class(a)
+a = 5L
+class(a)
+1/0
+1/Inf
+0/0
+x -> 6
+6 -> x
+x
+print(x)
+msg <- "hello"
+msg
+y  <- 1:20 # 1 to 20 numbers
+y
+m <- 1:10
+m
+dim(m) <- c(2,5)
+m
+dim(m)
+attributes(m)
+l <- matrix(nrow = 2, ncol = 3)
+l
+l <- matrix(1:6, nrow = 2, ncol = 3)
+l
