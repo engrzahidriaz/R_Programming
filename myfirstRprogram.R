4b2d8ece8052ed6d1b9be8cd5506c4b21b86bbf7
engrzahidriaz <zahidriaz371@gmail.com>
2021-01-19 11:30:56 +0500

    Add control structure

--- myfirstRprogram.R
+++ myfirstRprogram.R
@@ -1,3 +1,5 @@
+
+
 #----------- Data Types and Basic Operations -------------
 # ------------ Objects ----------
 
@@ -143,3 +145,75 @@ good <- complete.cases(x,y)
 good
 x[good]
 y[good]
+
+airquality
+View(airquality)
+a <- airquality
+airquality[1:6,]
+good <- complete.cases(airquality)
+good
+airquality[good,][1:6,]
+
+# ------------------- Control Structure -------------------
+
+# if/else condition
+x <- 4
+y <- 0
+if (x > 5){
+  y <- 10
+} else {
+  y <- 0
+}
+
+y <- if (x > 3) {
+  10
+} else {
+  0
+}
+
+ # for loop
+for (i in 1:4){
+  print(i)
+}
+
+#for loop for vector
+x <- c('a','b','c','d')
+for (i in 1:4){
+  print(x[i])
+}
+
+for (i in seq_along(x)){
+  print(x[i])
+}
+
+for (letter in x){
+  print(letter)
+}
+
+for (i in 1:4) print(x[i])
+
+# for loop for matrix
+y <- matrix(1:6, 2,6)
+for (i in seq_along(nrow(y))){
+  for (j in seq_along(ncol(y))){
+    print(y[i,j])
+  }
+}
+
+# while loop
+count <- 0
+while(count < 10){
+  print(count)
+  count <- count +1
+}
+
+z <- 5
+while(z >= 3 && z <= 10) {
+  print(z)
+  coin <- rbinom(1, 1, 0.5)
+  if(coin == 1) {
+    z <- z + 1
+  } else {
+    z <- z - 1
+  }
+}
