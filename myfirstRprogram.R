75f9af1b39543cd7f6fb914568be424cff9fc297
engrzahidriaz <zahidriaz371@gmail.com>
2021-01-21 10:20:01 +0500

    Add Date and Time

--- myfirstRprogram.R
+++ myfirstRprogram.R
@@ -274,3 +274,45 @@ y
 x * y
 x / y
 x %*% y    # true matrix Multiplication
+
+#------------------------ Date and Time -----------------
+
+as.Date('1970-01-01')
+as.POSIXct('1970-01-01')
+as.POSIXlt('1970-01-01')
+
+x <- Sys.time()
+x
+p <- as.POSIXlt(x)
+p
+names(unclass(p))
+p$sec
+p$mon
+p$wday
+
+x <- Sys.time()
+x                   # Already in ‘POSIXct’ format
+x$sec
+
+datestring <- c("January 10, 2012 10:40",
+                "December 9, 2011 9:10")
+x <- strptime(datestring, "%B %d, %Y %H:%M")
+x
+class(x)
+
+x <- as.Date("2012-01-01")
+y <- strptime("9 Jan 2011 11:34:21", "%d %b %Y %H:%M:%S")
+x-y
+class(x)
+class(y)
+x <- as.POSIXlt(x)
+x-y
+
+x <- as.Date("2012-03-01")
+y <- as.Date("2012-02-28")
+x-y
+
+x <- as.POSIXct("2012-10-25 01:00:00")
+y <- as.POSIXct("2012-10-25 06:00:00", tz = "GMT")
+y-x
+x
